73ccbd6df9ca02c0be9f854f1d8a0de4885d5d81
mateopinto0 <pintomateo74@gmail.com>
2026-04-04 01:07:53 -0300

    ejercicio de juego de numero resuelto, agregado de test y de interfaz juego, separacion de logica.

---
+++ Clase1-Tarea/Clase-1-Tarea/Clase-1-Tarea/Program.cs
@@ -0,0 +1,27 @@
+﻿using Clase_1_Tarea.JuegoNumero;
+using System.Security.Cryptography;
+
+string resultado = "";
+var juego = new JuegoNumero(55);
+
+do
+{
+
+    Console.WriteLine("Ingrese un numero entre 1 y 100:");
+    string input = Console.ReadLine();
+    int valorJugador = int.Parse(input);
+
+    
+    resultado = juego.resultadoJuegoNumero(valorJugador);
+
+    Console.WriteLine(resultado);
+
+} while (resultado != "Ganaste");
+
+if(resultado == "Ganaste") {
+    Console.WriteLine("Lo has logrado en el intento " + juego.getIntentos);
+}
+    
+
+
+
